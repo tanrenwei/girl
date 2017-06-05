c0c2344d160c7a7764b3856d827932756beeed37
tanrenwei <549198212@qq.com>
2017-06-05 15:38:40 +0800

    Initial commit

---
+++ src/main/java/com/tanrw/GirlProperties.java
@@ -0,0 +1,38 @@
+package com.tanrw;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Administrator on 2017-5-26.
+ */
+@ConfigurationProperties(prefix = "girl1")
+@Component
+public class GirlProperties {
+    private String cupsize;
+    private int age;
+
+    public String getCupsize() {
+        return cupsize;
+    }
+
+    public void setCupsize(String cupsize) {
+        this.cupsize = cupsize;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "GirlProperties{" +
+                "cupsize='" + cupsize + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
---
+++ src/main/java/com/tanrw/HelloController.java
@@ -0,0 +1,22 @@
+package com.tanrw;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by Administrator on 2017-5-25.
+ */
+@Controller
+public class HelloController {
+
+    @Autowired
+    private GirlProperties girlProperties;
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String say() {
+        return "index";
+
+    }
+}
